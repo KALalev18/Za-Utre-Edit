5428b1c08723848b7d0da1d74ad43f835d3f44c7
KALalev18 <59725875+KALalev18@users.noreply.github.com>
2022-06-03 14:10:13 +0300

    Delete DBProject directory

--- DBProject/src/Utils/ApplicationProperties.java
+++
@@ -1,5 +0,0 @@
-package Utils;
-
-public class ApplicationProperties {
-	public static final String url = "jdbc:sqlserver://SD2324\\sqlexpress;databaseName=Project_Database;integratedSecurity=true";
-}
